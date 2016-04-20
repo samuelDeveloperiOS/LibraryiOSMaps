a43007352dcdcf305d829a4fc59f6aee67bacc1f
Samuel Romero <samuelromero@mac-mini-de-samuel.local>
2016-04-20 10:34:54 -0500

    Initial Commit

---
+++ libMapiOS/libMapiOS.h
@@ -0,0 +1,13 @@
+//
+//  libMapiOS.h
+//  libMapiOS
+//
+//  Created by Samuel Romero on 20/04/16.
+//  Copyright © 2016 Samuel Romero. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libMapiOS : NSObject
+
+@end
---
+++ libMapiOS/libMapiOS.m
@@ -0,0 +1,13 @@
+//
+//  libMapiOS.m
+//  libMapiOS
+//
+//  Created by Samuel Romero on 20/04/16.
+//  Copyright © 2016 Samuel Romero. All rights reserved.
+//
+
+#import "libMapiOS.h"
+
+@implementation libMapiOS
+
+@end
